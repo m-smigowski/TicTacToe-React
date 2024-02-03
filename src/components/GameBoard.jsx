9a00b4ccd9db8a88706374517f754d3f67a9f45a
m-smigowski <smigowski.marcin@gmail.com>
2024-02-03 18:14:25 +0100

    the log compontent was added and removed unnecessary state in gameboard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ]
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-    const[gameBoard, setGameBoard] = useState(initialGameBoard)
+export default function GameBoard({ onSelectSquare,turns }) {
+    let gameBoard = initialGameBoard;
+
+    for(const turn of turns){
+        const { square, player} = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
+
+
+
+    /*const[gameBoard, setGameBoard] = useState(initialGameBoard)
 
     function handleSelectSquare(rowIndex, colIndex){
         setGameBoard((prevGameBoard) =>{
@@ -17,14 +26,14 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
         });
 
         onSelectSquare();
-    }
+    }*/
     
     return <ol id="game-board">
         {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol, colIndex) => 
                 <li key={colIndex}>
-                    <button onClick={() => handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button> </li>)}
+                    <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button> </li>)}
             </ol>
         </li>)}
     </ol>
